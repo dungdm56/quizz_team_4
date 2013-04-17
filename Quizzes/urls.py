9a1544e8b8ae33a63af9df4fe043a68d8496fe8d
dainv560 <dainv_560@vnu.edu.vn>
2013-04-17 21:48:57 +0700

    change comment for code in file in Quizzes 17-4

--- Quizzes/urls.py
+++ Quizzes/urls.py
@@ -19,39 +19,42 @@ from Quizz.views import *
 from Quizzes.views  import home
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Quizzes.views.home', name='home'),
-    # url(r'^Quizzes/', include('Quizzes.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+	#home
     url(r'^$', home),
 	url(r'^home/$', home),
-    url(r'^admin/', include(admin.site.urls)),
+	
+	#account
     url(r'^login/$', login_user),
     url(r'^logout/$', logout),
     url(r'^register/$', signup),
     url(r'./register/$', signup),
-    url(r'^create_class/$', create_class),
-    url(r'^create_class_success/([^\s]+)/$', create_class_success),
-    url(r'^create_quizz/([^\s]+)/$', create_quizz),
-    url(r'^create_questions/([^\s]+)/$', create_question),
-    url(r'^class/([^\s]+)/$', classes),
-    url(r'^quizzes/([^\s]+)/$', quizzes),
-    url(r'^doing_quizz/([^\s]+)/$', doing_quizz),
-    url(r'^result/([^\s]+)/([^\s]+)/([^\s]+)/([^\s]+)/$', result),
-    url(r'^answer/([^\s]+)/$', answer),
-    url(r'^edit_class/([^\s]+)/$', edit_class),
     url(r'^about/$', about),
     url(r'^signup_success/$', signup_success),
-	url(r'^edit_quizz/([^\s]+)/$', edit_quizz),
-	url(r'^edit_question/([^\s]+)/$', edit_question),
-	url(r'^delete_question/([^\s]+)/$', delete_question),
-	
     url(r'^member/', include('accounts.urls')),
 	
+	#classes
+    url(r'^class/([^\s]+)/$', classes),
+    url(r'^create_class/$', create_class),
+    url(r'^create_class_success/([^\s]+)/$', create_class_success),
+    url(r'^edit_class/([^\s]+)/$', edit_class),
+	
+	#quizzes
+    url(r'^create_quizz/([^\s]+)/$', create_quizz),
+    url(r'^quizzes/([^\s]+)/$', quizzes),
+    url(r'^doing_quizz/([^\s]+)/$', doing_quizz),
+	url(r'^edit_quizz/([^\s]+)/$', edit_quizz),
+	
+	#question and result
+    url(r'^create_questions/([^\s]+)/$', create_question),
+	url(r'^edit_question/([^\s]+)/$', edit_question),
+	url(r'^delete_question/([^\s]+)/$', delete_question),
+    url(r'^result/([^\s]+)/([^\s]+)/([^\s]+)/([^\s]+)/$', result),
+    url(r'^answer/([^\s]+)/$', answer),
 	
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve',
          { 'document_root': static }),
--- Quizzes/views.py
+++ Quizzes/views.py
@@ -18,12 +18,16 @@ from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
 
+#the definition for home method
 def home(request):
-
+	#check if the user is valid or not
     if request.user is not None :
+		#get some variable for context in web page
         classes_list = Classes.objects.filter(user = request.user.id)
         classes_list_joined = Classes.objects.filter(students = request.user.id)
         classes_list_another = Classes.objects.all()
+		
+		#return home.html and some context
         return render_to_response('home.html', context_instance = RequestContext(request,{'User':request.user,'classes_list':classes_list,'classes_list_another':classes_list_another,'classes_list_joined':classes_list_joined}))
     else :
-        return render_to_response('home.html', context_instance = RequestContext(request,{'User':request.user}))
+        return render_to_response('home.html', context_instance = RequestContext(request,{'User':request.user}))	#return home.html and some context
